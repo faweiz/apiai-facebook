9ea8234f88a9a0bfc32fefc55f15cab212b00ce6
Faweiz <faweiz@gmail.com>
2020-03-15 14:36:21 -0400

    Update message.js

--- db/message.js
+++ db/message.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-//mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect('mongodb://tonyz:123456789@ds051655.mlab.com:51655/apiai-faweizfacebook', {useMongoClient: true, useNewUrlParser: true});
+mongoose.connect(process.env.MONGOLAB_URI, {useMongoClient: true, useNewUrlParser: true});
 const db = mongoose.connection;
 const Schema = mongoose.Schema;
 const messageSchema = new Schema({
